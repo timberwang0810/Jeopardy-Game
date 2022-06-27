b4e7dd0ca07f656667e3fa1fe442e1fb4e9d2c4f
danny4343 <dansunko@gmail.com>
2022-06-27 16:01:14 -0400

    Now interactive!

--- client/vis.js
+++ client/vis.js
@@ -24,102 +24,69 @@ function createTable(data){
 
     column.append('div')
         .attr('class', 'cell question')
+        .attr('category', d => d.category)
         .attr('value', '200')
         .text('$200')
 
     column.append('div')
         .attr('class', 'cell question')
+        .attr('category', d => d.category)
         .attr('value', '400')
         .text('$400')
 
     column.append('div')
         .attr('class', 'cell question')
+        .attr('category', d => d.category)
         .attr('value', '600')
         .text('$600')
 
     column.append('div')
         .attr('class', 'cell question')
+        .attr('category', d => d.category)
         .attr('value', '800')
         .text('$800')
 
     column.append('div')
         .attr('class', 'cell question')
+        .attr('category', d => d.category)
         .attr('value', '1000')
         .text('$1000')
 
+
+    // make pointer
+	const mover = function() {
+		d3.select(this)
+			.style("cursor", "pointer");
+	}
+	// on mouse out restore defaults
+	const mout = function() {
+		d3.select(this)
+			.style("cursor", "default");
+	}
+	// on click call up question
+	const mclick = function(){
+        const question = data.filter(d => d.category==this.getAttribute("category"))[0][this.getAttribute('value')].question
+        d3.select('.textbox').text(question)
+	}
+    // on double click call up answer
+    const dblclick = function(){
+        const answer = data.filter(d => d.category==this.getAttribute("category"))[0][this.getAttribute('value')].answer
+        d3.select('.textbox').text(answer)
+	}
+    const clear = function(){
+        d3.select('.textbox').text('')
+    }
+
+    d3.select('#clear').on('click', clear)
+
+    const cells = d3.selectAll('.question')
+	cells.on("mouseover", mover).on("mouseout", mout).on('click', mclick).on('dblclick', dblclick);
  
 
 }
+
 
 createTable(test.game)
 
 console.log(test)
 
-
-
-// function notcreateTable(mycontainer, tableheader, tabledata) {
-// 	/*
-// 	Create a quasi table. ("quasi" because use HTML <div> tags, not <table> tags)
-
-// 	@param {string} mycontainer An (HTML node) element to which to attach quasi-table
-// 	@param {json} tableheader A JS object which includes the keys: header, type, size
-// 	@param {json} tabledata A JS object comprising the table data
-// 	*/
-
-// 	// select container
-// 	const table = d3.select(mycontainer)
-
-// 	// add header div
-// 	const headerDiv = table.append('div')
-// 		.attr('class', 'flxh-s')
-
-// 	// get size as string from header
-// 	const cellSize = 15;
-
-// 	// Add child items to header (i.e., header column titles) into header div
-// 	const header = headerDiv.selectAll('cells')
-// 		.data(tableheader.header)
-// 		.enter()
-// 		.append('div')
-// 		.attr('class', d => `cell-header ${cellSize(d)}`) 
-// 		.attr('value', d => d)
-// 		.text(d => d)
-// 		.on('click', sorter)
-
-// 	// create body rows
-// 	const bodyRows = table.selectAll('rows')
-// 		.data(tabledata)
-// 		.enter()
-// 		.append('div')
-// 		.attr('class', 'flxh-s js-selector-row')
-// 		.attr('value', d => d.species_taxid)
-
-// 	// fill with cells
-// 	const bodyCells = bodyRows.selectAll('cells')
-// 		.data(d => Object.values(d).entries()) // returns array: [index, data]
-// 		.enter()
-// 		.append('div')
-// 		.attr('class', d => `cell ${cellSizeIndex(d[0])}`)
-// 		.text(d => d[1])
-
-// 	// interactive
-// 	const in_dict = data_dict.map(d => d.taxid) // list of taxids in dict
-// 	const existing = bodyRows.filter(d => in_dict.includes(d.species_taxid)) // rows for those taxids
-
-// 	// make pointer
-// 	const mover = function() {
-// 		d3.select(this)
-// 			.style("cursor", "pointer");
-// 	}
-// 	// on mouse out restore defaults
-// 	const mout = function() {
-// 		d3.select(this)
-// 			.style("cursor", "default");
-// 	}
-// 	// on click call pop up
-// 	const mclick = function(){
-// 		getTop = el => el.offsetTop + (el.offsetParent && getTop(el.offsetParent))
-// 		pop_up(data_dict,'taxid', this.getAttribute("value"), getTop(this))
-// 	}
-// 	existing.on("mouseover", mover).on("mouseout", mout).on('click', mclick);
-// }
